0d2a9ff9c709c672c66caf31a65e0a458072ef7c
Samuel Gomes Vieira <samuel.gomes@uds.com.br>
2025-02-02 20:52:37 -0300

    ci: configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,60 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintPluginImportHelpers from 'eslint-plugin-import-helpers'
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ["node_modules", "eslint.config.mjs", "*.json"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    plugins: {
+      'import-helpers': eslintPluginImportHelpers
+    },
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
-      sourceType: 'module',
+      ecmaVersion: 'latest',
+      sourceType: "module",
       parserOptions: {
+        project: "tsconfig.json",
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
+        sourceType: "module",
       },
     },
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      'import-helpers/order-imports': [
+        'warn',
+        {
+          'newlinesBetween': 'always',
+          'groups': [
+            'module',
+            '/^@\\//',
+            [
+              'parent',
+              'sibling'
+            ],
+            'index'
+          ],
+          'alphabetize': {
+            'order': 'asc',
+            'ignoreCase': true
+          }
+        }
+      ],
     },
   },
 );
